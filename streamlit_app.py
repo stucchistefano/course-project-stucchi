5818a068699bb75851a9f3e25e9d4b9f7369b5e5
stucchistefano <stefano4.stucchi@mail.polimi.it>
2025-04-07 10:25:01 +0000

    I did some changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,7 @@ st.set_page_config(
 # Definition of wanted font and colours
 primary_color = "#A1289B"  # Esempio: Viola Scuro per i titoli
 secondary_color = "#A1289B"  # Esempio: Viola Scuro per il testo normale
+white_color = "#FFFFFF" # White
 accent_color = "#000000"   # Esempio: Nero per i sottotitoli
 main_font = "sans-serif"    # Esempio: Font sans-serif
 title_font = "sans-serif"       # Esempio: Font serif per i titoli
@@ -24,7 +25,7 @@ custom_css = f"""
     opacity: 1;
 }}
 
-h1, h2, h3 {{
+h1, h2 {{
     color: {primary_color};
     font-family: {title_font};
     text-align: center;
@@ -37,7 +38,21 @@ h4 {{
     text-align: center;
 }}
 
-p, div, ul, ol, li {{
+p {{
+    color: {white_color};
+    font-family: {main_font};
+    text-align: center;
+    font-size: 1.3rem !important;
+}}
+
+ul, ol, li, h3 {{
+    color: {white_color};
+    font-family: {main_font};
+    text-align: center;
+}}
+
+
+div {{
     color: {secondary_color};
     font-family: {main_font};
     text-align: center;
@@ -99,7 +114,11 @@ st.markdown(custom_css, unsafe_allow_html=True)
 # Use the custom class in a container
 st.markdown('<div class="my-container">The "must-have" app for <br> coaches & athletes </div>', unsafe_allow_html=True)
 
+
 # Buttons
+# Apply the custom CSS
 left, middle, right = st.columns(3)
-if middle.button("Discover KPills", use_container_width=True):
-    middle.markdown("You clicked the emoji button.")
+middle.button("Discover KPills", use_container_width=False)
+    #middle.markdown('<p style="color:purple; font-size:20px;">Button Clicked</p>', unsafe_allow_html=True)
+#if middle.button("Login", use_container_width=False):
+middle.button("Login", use_container_width=False)
